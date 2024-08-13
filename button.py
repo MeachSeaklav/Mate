3d072402fa537e6fbc05fc190009e3bc59e3d2a8
MeachSeaklav <143688280+MeachSeaklav@users.noreply.github.com>
2024-08-13 14:32:55 +0700

    Update button.py

--- button.py
+++ button.py
@@ -311,6 +311,15 @@ def create_zip_file(word_filename, pdf_filename, zip_filename):
     except Exception as e:
         st.error(f"Failed to create zip file: {e}")
 
+# def create_zip_file(word_filename, pdf_filename, zip_filename):
+#     try:
+#         with zipfile.ZipFile(zip_filename, 'w') as zipf:
+#             zipf.write(word_filename)
+#             zipf.write(pdf_filename)
+#         st.success(f"Zip file {zip_filename} created successfully.")
+#     except Exception as e:
+#         st.error(f"Failed to create zip file: {e}")
+
 def send_email_with_attachments(subject, body, attachments):
     to_email = ["hratana261@gmail.com", "khengdalish21@gmail.com", "chlakhna702@gmail.com"]
 
